ca85b0443efc6524ebbab9e4a96a88a139b7dd3d
hanteng <hanteng@kylinos.cn>
2023-05-24 10:34:35 +0800

    优化代码，增加FCITX_ADDON_FACTORY

--- src/sogouim.cpp
+++ src/sogouim.cpp
@@ -1,1 +1,14 @@
 #include "sogouim.h"
+
+namespace fcitx {
+SogouIMEngine::SogouIMEngine(Instance *instance) : instance_(instance) {}
+
+SogouIMEngine::~SogouIMEngine() {}
+
+void SogouIMEngine::keyEvent(const InputMethodEntry &entry,
+                             KeyEvent &keyEvent) {
+    std::cout << "key = " << std::endl;
+}
+} // namespace fcitx
+
+FCITX_ADDON_FACTORY(fcitx::SogouIMEngineFactory)
--- src/sogouim.h
+++ src/sogouim.h
@@ -3,4 +3,32 @@
 
 #include <iostream>
 
+#include <fcitx/addonfactory.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputmethodengine.h>
+#include "fcitx/addoninstance.h"
+
+namespace fcitx {
+
+class SogouIMEngine final : public InputMethodEngineV3 {
+public:
+    SogouIMEngine(Instance *instance);
+    ~SogouIMEngine();
+    Instance *instance() { return instance_; }
+    void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) override;
+
+private:
+    Instance *instance_;
+};
+
+class SogouIMEngineFactory : public AddonFactory {
+public:
+    AddonInstance *create(AddonManager *manager) override {
+        FCITX_INFO() << "SogouIMEngineFactory ...";
+        return new SogouIMEngine(manager->instance());
+    }
+};
+
+} // namespace fcitx
+
 #endif
